f5b2ffe1d924cca0953f33ff70f90839fbf86be4
SebastianPal <spalewski@gmail.com>
2019-05-04 01:23:02 +0200

    ipCheck added

--- ipinfo/main.go
+++ ipinfo/main.go
@@ -4,37 +4,43 @@ import (
 	"encoding/json"
 	"flag"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"strconv"
 )
 
 func CheckIp(toCheck string, geo bool) string {
-	var fields string = "?fields=1273"
-	var ipApi = "http://ip-api.com/json/"
-	resp, err := http.Get(ipApi + toCheck + fields)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	var response Response
-	json.Unmarshal(body, &response)
-
-	var ipAdress string = "IP address: " + toCheck
-	var organization string = "Organization: " + response.Org
-	var city string = "City: " + response.City
-	var region string = "Region: " + response.RegionName
-	var country string = "Country: " + response.Country
-	var loc string = "Loc: " + FloatToString(response.Lat) + ", " + FloatToString(response.Lon)
-	var postal string = "Postal: " + response.Zip
 	var ipInfo string
 
-	if geo == true {
-		ipInfo = ipAdress + "\n" + city + "\n" + region + "\n" + country + "\n" + loc + "\n" + postal
-	} else {
+	if IsIPCorrect(toCheck) == true {
+		var fields string = "?fields=1273"
+		var ipApi = "http://ip-api.com/json/"
+		resp, err := http.Get(ipApi + toCheck + fields)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
 
-		ipInfo = ipAdress + "\n" + organization + "\n" + city + "\n" + region + "\n" + country + "\n" + loc + "\n" + postal
+		body, err := ioutil.ReadAll(resp.Body)
+		var response Response
+		json.Unmarshal(body, &response)
+
+		var ipAdress string = "IP address: " + toCheck
+		var organization string = "Organization: " + response.Org
+		var city string = "City: " + response.City
+		var region string = "Region: " + response.RegionName
+		var country string = "Country: " + response.Country
+		var loc string = "Loc: " + FloatToString(response.Lat) + ", " + FloatToString(response.Lon)
+		var postal string = "Postal: " + response.Zip
+
+		if geo == true {
+			ipInfo = ipAdress + "\n" + city + "\n" + region + "\n" + country + "\n" + loc + "\n" + postal
+		} else {
+
+			ipInfo = ipAdress + "\n" + organization + "\n" + city + "\n" + region + "\n" + country + "\n" + loc + "\n" + postal
+		}
+	} else {
+		ipInfo = "Entered address " + toCheck + " is not valid"
 	}
 
 	return ipInfo
@@ -53,6 +59,13 @@ type Response struct {
 	RegionName string        `json:"regionName"`
 	Zip        string        `json:"zip"`
 	IpCheck    []interface{} `json:"ipCheck"`
+}
+
+func IsIPCorrect(givenIp string) bool {
+	if net.ParseIP(givenIp) == nil {
+		return false
+	}
+	return true
 }
 
 func main() {
